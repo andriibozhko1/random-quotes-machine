819414ec395b292e92deff821923a726c00ab946
Andrii <andriibozhko1@gmail.com>
2019-03-11 23:04:59 +0200

    to gh-pages

---
+++ precache-manifest.19ac8b7563d8084ff4c4050956b30dca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "27fcc125868d879b8abe",
+    "url": "/random-quotes-machine/static/js/0.83cc52e8.chunk.js"
+  },
+  {
+    "revision": "acbfc27024f21cd20ac3",
+    "url": "/random-quotes-machine/static/css/main.6d669775.chunk.css"
+  },
+  {
+    "revision": "acbfc27024f21cd20ac3",
+    "url": "/random-quotes-machine/static/js/main.b98c7a2a.chunk.js"
+  },
+  {
+    "revision": "d0002ac30f1881a4debb",
+    "url": "/random-quotes-machine/static/js/runtime~main.3e013a71.js"
+  },
+  {
+    "revision": "bdc9063513ee3d9becb2",
+    "url": "/random-quotes-machine/static/js/3.38f51d39.chunk.js"
+  },
+  {
+    "revision": "4ad5c94699f8aa7a7f9a",
+    "url": "/random-quotes-machine/static/css/4.8b5f4fac.chunk.css"
+  },
+  {
+    "revision": "4ad5c94699f8aa7a7f9a",
+    "url": "/random-quotes-machine/static/js/4.c3cc20c8.chunk.js"
+  },
+  {
+    "revision": "8014e7695124092d9db6",
+    "url": "/random-quotes-machine/static/css/5.b478d98d.chunk.css"
+  },
+  {
+    "revision": "8014e7695124092d9db6",
+    "url": "/random-quotes-machine/static/js/5.f9d10749.chunk.js"
+  },
+  {
+    "revision": "a0cceb16e942df7123a47b22af3cea3e",
+    "url": "/random-quotes-machine/index.html"
+  }
+];
